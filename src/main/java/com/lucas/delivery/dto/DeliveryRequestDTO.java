bffb230580c2e509a33600dddebf13b4f9e31ecc
Lucas <diniz3003@gmail.com>
2024-12-28 08:11:14 -0300

    adjustement /list

--- src/main/java/com/lucas/delivery/dto/DeliveryRequestDTO.java
+++ src/main/java/com/lucas/delivery/dto/DeliveryRequestDTO.java
@@ -6,9 +6,17 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record DeliveryRequestDTO(
-        @NotBlank(message = "O setor é obrigatório") String sector,
+        @NotBlank(message = "O setor é obrigatório")  String sector,
         @NotBlank(message = "A empresa é obrigatória") String enterprise,
         @NotNull(message = "A data é obrigatória") LocalDate previewData,
         String recipientName,
         String deliveryCode
-){}
+){
+
+    public DeliveryRequestDTO {
+        sector = sector.toUpperCase();
+        enterprise = enterprise.toUpperCase();
+        if (recipientName != null) recipientName = recipientName.toUpperCase();
+        if (deliveryCode != null) deliveryCode = deliveryCode.toUpperCase();
+    }
+}
